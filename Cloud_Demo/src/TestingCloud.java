2535c851eec453bee561ec6ddd9ce1bb5557eb75
CHINAR <CHINAR@chinar-d11572d8>
2015-07-11 18:11:10 +0530

    Adding the method

--- Cloud_Demo/src/TestingCloud.java
+++ Cloud_Demo/src/TestingCloud.java
@@ -1,9 +1,15 @@
 
 public class TestingCloud {
+	
+	 void display()
+	{
+		System.out.println("From Display method");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing");
+		System.out.println("Testing1");
 
 	}
 
